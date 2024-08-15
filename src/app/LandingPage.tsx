72fd8aa28fb2d7b72e850db2b1fc2d0c5d43d3d7
yanjiayun <Anyayan168@gmail.com>
2024-08-15 10:46:48 -0400

    feat: landing page beginning

---
+++ src/app/LandingPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import LandingHeader from "../../components/landingPage/LandingHeader";
+import Mission from "../../components/landingPage/Mission";
+
+const LandingPage = () => {
+  return (
+    <>
+      <LandingHeader />
+      <Mission />
+    </>
+  )
+};
+
+export default LandingPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import LandingPage from './LandingPage';
+import NavigationBar from '../../components/NavigationBar';
+// import Footer from '../../components/Footer'
 
 export default function Home() {
   return (
-    <>Hello</>
+    <>
+      <NavigationBar />
+      <LandingPage/>
+      {/* <Footer /> */}
+    </>
   );
 }
